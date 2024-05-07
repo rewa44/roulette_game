180001c3cd4ad86cfbc5ef99b6f28d60961e3999
rewa44 <168294248+rewa44@users.noreply.github.com>
2024-05-07 08:45:23 -0400

    Add files via upload
    5/6 save tomight

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 import random
 from roulette import Roulette
+from button import Button
 pygame.init()
 pygame.font.init()
 my_font = pygame.font.SysFont('Arial', 15)
 ny_font = pygame.font.SysFont('Arial', 45)
-SCREEN_HEIGHT = 700
-SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 900
+SCREEN_WIDTH = 1200
 size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
 r = 58
@@ -15,20 +16,30 @@ b = 130
 run = True
 title = True
 gamek = False
-rumz = Roulette(10, 350)
+rumz = Roulette(0, 900-406)
+coords = []
+for i in range(12):
+    coords.append((140+45*i, 560))
+    coords.append((140 + 45 * i, 620))
+    coords.append((140 + 45 * i, 680))
+print(coords)
+slunk = Button(185,620,1)
 while run:
+    pos = pygame.mouse.get_pos()
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
         if (event.type == pygame.KEYUP or event.type == pygame.MOUSEBUTTONUP) and title:
             title = False
             gamek = True
-
+        if event.type == pygame.MOUSEBUTTONUP and slunk.rect.collidepoint(pos):
+                print("Hit")
     if title:
         screen.fill((r, g, b))
         # add opening image
         pygame.display.update()
     if gamek:
-        screen.fill((r+180, g+40, b+20))
+        screen.fill((r+180, g+40, b+20)) # placeholder
         screen.blit(rumz.image, rumz.rect)
+        screen.blit(slunk.image, slunk.rect)
         pygame.display.update()
--- roulette.py
+++ roulette.py
@@ -6,4 +6,7 @@ class Roulette:
         self.y = y
         self.image = pygame.image.load("roulette.png")
         self.image_size = self.image.get_size()
+        scale_size = (self.image_size[0] * 1.3, self.image_size[1] * 1.3)
+        self.image = pygame.transform.scale(self.image, scale_size)
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.image_size = self.image.get_size()
